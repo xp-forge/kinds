1b933497516eccd02dcd80114af11d9d2e6799c6
Timm Friebe <thekid@thekid.de>
2015-09-27 17:16:23 +0200

    Remove sortable - replaced by lang.partial.CompareTo

--- src/test/php/lang/partial/unittest/Named.class.php
+++ src/test/php/lang/partial/unittest/Named.class.php
@@ -1,14 +1,14 @@
 <?php namespace lang\partial\unittest;
 
 use lang\partial\Identity;
-use lang\partial\Sortable;
+use lang\partial\CompareTo;
 
 /**
- * Used as fixture in the "IdentityTest" and "SortableTest" classes
+ * Used as fixture in the "IdentityTest" and "CompareToTest" classes
  */
 class Named extends \lang\Object implements \lang\Value {
   use Named\is\Identity { value as name; }
-  use Named\including\Sortable;
+  use Named\including\CompareTo;
 
   /** @return bool */
   public function isEmpty() { return '' === $this->value; }
--- src/test/php/lang/partial/unittest/Person.class.php
+++ src/test/php/lang/partial/unittest/Person.class.php
@@ -2,16 +2,16 @@
 
 use lang\partial\ValueObject;
 use lang\partial\Constructor;
-use lang\partial\Sortable;
+use lang\partial\CompareTo;
 use lang\partial\Comparators;
 
 /**
- * Used by SortableTest
+ * Used by CompareToTest
  */
 class Person extends \lang\Object {
   use Person\including\ValueObject;
   use Person\including\Constructor;
-  use Person\including\Sortable;
+  use Person\including\CompareTo;
   use Person\including\Comparators {
     byBorn as byBirthDate;
   }
