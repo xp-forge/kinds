61bd32928653716fd9a69101dbaf37d24a0f217f
Timm Friebe <thekid@thekid.de>
2015-09-27 16:20:16 +0200

    Add lang.partial.Equals transformation

--- src/test/php/lang/partial/unittest/AccessorsTest.class.php
+++ src/test/php/lang/partial/unittest/AccessorsTest.class.php
@@ -5,9 +5,9 @@ class AccessorsTest extends PartialTest {
   #[@test, @values(['private', 'protected', 'public'])]
   public function generates_accessor_method_for_instance_field($modifier) {
     $fixture= $this->declareType(sprintf('{
-      %s $name;
-
       use <T>\with\lang\partial\Accessors;
+
+      %s $name;
     }', $modifier));
     $this->assertDeclaresMethods(['public var name()'], $fixture);
   }
@@ -15,9 +15,9 @@ class AccessorsTest extends PartialTest {
   #[@test]
   public function does_not_generate_accessors_for_static_fields() {
     $fixture= $this->declareType('{
-      static $name;
-
       use <T>\with\lang\partial\Accessors;
+
+      static $name;
     }');
     $this->assertDeclaresMethods([], $fixture);
   }
@@ -25,10 +25,10 @@ class AccessorsTest extends PartialTest {
   #[@test]
   public function generates_accessor_methods_in_order_fields_are_declared() {
     $fixture= $this->declareType('{
+      use <T>\with\lang\partial\Accessors;
+
       private $name, $handle;
       private $id= -1;
-
-      use <T>\with\lang\partial\Accessors;
     }');
     $this->assertDeclaresMethods(['public var name()', 'public var handle()', 'public var id()'], $fixture);
   }
---
+++ src/test/php/lang/partial/unittest/EqualsTest.class.php
@@ -0,0 +1,40 @@
+<?php namespace lang\partial\unittest;
+
+class EqualsTest extends PartialTest {
+  private $fixture;
+
+  public function setUp() {
+    $this->fixture= $this->declareType('{
+      use <T>\with\lang\partial\Equals;
+
+      private $id, $name, $skills;
+      public function __construct($id, $name, $skills= []) {
+        $this->id= $id;
+        $this->name= $name;
+        $this->skills= $skills;
+      }
+    }');
+  }
+
+  #[@test]
+  public function equals_itself() {
+    $instance= $this->fixture->newInstance(6100, 'Test', ['Dev']);
+    $this->assertEquals($instance, $instance);
+  }
+
+  #[@test]
+  public function equals_instance_with_equal_members() {
+    $this->assertEquals(
+      $this->fixture->newInstance(6100, 'Test', ['Dev']),
+      $this->fixture->newInstance(6100, 'Test', ['Dev'])
+    );
+  }
+
+  #[@test]
+  public function does_not_equal_instance_with_unequal_members() {
+    $this->assertNotEquals(
+      $this->fixture->newInstance(6100, 'Test', ['Dev']),
+      $this->fixture->newInstance(61, 'Other', [])
+    );
+  }
+}
