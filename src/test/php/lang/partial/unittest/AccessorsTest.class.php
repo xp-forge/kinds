f1d4871bdde08a9c41463dc3d69ae70018332f3b
Timm Friebe <thekid@thekid.de>
2015-09-27 15:50:26 +0200

    Test no accessors are generated for static fields

--- src/test/php/lang/partial/unittest/AccessorsTest.class.php
+++ src/test/php/lang/partial/unittest/AccessorsTest.class.php
@@ -3,13 +3,23 @@
 class AccessorsTest extends PartialTest {
 
   #[@test, @values(['private', 'protected', 'public'])]
-  public function generates_accessor_method_for_field($modifier) {
+  public function generates_accessor_method_for_instance_field($modifier) {
     $fixture= $this->declareType(sprintf('{
       %s $name;
 
       use <T>\with\lang\partial\Accessors;
     }', $modifier));
     $this->assertDeclaresMethods(['public var name()'], $fixture);
+  }
+
+  #[@test]
+  public function does_not_generate_accessors_for_static_fields() {
+    $fixture= $this->declareType('{
+      static $name;
+
+      use <T>\with\lang\partial\Accessors;
+    }');
+    $this->assertDeclaresMethods([], $fixture);
   }
 
   #[@test]
